617a889557b6127f385874d5a5dfc47d98c63154
SSK Opensource <opensource.ssk@gmail.com>
2022-02-13 15:00:17 +0530

    Add project files.

---
+++ SampleApp/ViewModel/ViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleApp.ViewModel
+{
+    public class ViewModel:INotifyPropertyChanged
+    {
+        private string m_firstName;
+        private string m_LastName;
+
+        public ViewModel()
+        {
+            FirstName = "Deepti";
+            LastName = "Gupta";
+            Console.WriteLine(FirstName);
+        }
+
+
+        public string FirstName
+        {
+            get
+            {
+                return m_firstName;
+            }
+            set
+            {
+                m_firstName = value;
+                OnPropertyChanged(nameof(FirstName));
+                OnPropertyChanged(nameof(FullName));
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return m_LastName;
+            }
+            set
+            {
+                m_LastName = value;
+                OnPropertyChanged(nameof(LastName));
+                OnPropertyChanged(nameof(FullName));
+            }
+        }
+        
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
